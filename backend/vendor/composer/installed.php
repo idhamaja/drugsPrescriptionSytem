3064f097c0e13221c2ed71e31202460b8d0ff16b
Idham Ganteng <idham16iramap.com>
2024-10-20 00:27:49 +0700

    update TODAY 20-10-2024 Sunday

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '02f7cdbbf39b964171d23732867bac16ce531e40',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '02f7cdbbf39b964171d23732867bac16ce531e40',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'kodols/php-mysql-library' => array(
+            'pretty_version' => 'v1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => '2ee56d8e0e686991e1512d92b8596bd2ab0ecdd8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kodols/php-mysql-library',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
